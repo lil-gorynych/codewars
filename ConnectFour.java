082a8333db928bdb70c72b64fde5bb563e89ddcc
gorynych <ilya.goronok@icloud.com>
2020-08-06 22:38:06 +0300

    connectFour todo

---
+++ ConnectFour.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class ConnectFour {
+    public static String whoIsWinner(List<String> piecesPositionList) {
+        // retrun "Red" or "Yellow" or "Draw"
+        return "Draw";
+    }
+}
