610a106b86457acacd304e19e8cb3bd1dc42294f
Ilya Goronok <ilya.goronok@icloud.com>
2020-08-08 00:53:28 +0300

    is this the end?

--- Warrior.java
+++ Warrior.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Warrior {
     public int level;
     public int experience;
     public String rank;
+    public List<String> achievements = new ArrayList<>();
 
     final String[] ranks = new String[]{
             "Pushover", "Novice", "Fighter", "Warrior", "Veteran",
@@ -20,14 +24,25 @@ class Warrior {
 
 
         int diff = this.level - level;
+
         //if lower or equal
         if (diff == 0) { changeExperience(10); return "A good fight";}
         if (diff == 1) { changeExperience(5 ); return "A good fight";}
         if (diff >= 2) { changeExperience(0 ); return "Easy fight"; }
+
         //if higher
-        if ()
+        if (diff <= -5 || getIntRank(this.level) < getIntRank(level)) { return "You've been defeated"; }
+        changeExperience(20 * diff*diff);
+        return "An intense fight";
+    }
 
-
+    public String training (String description, int xp, int minLevel) {
+        if (this.level >= minLevel) {
+            this.achievements.add(description);
+            changeExperience(xp);
+            return description;
+        }
+        return "Not strong enough";
     }
 
     private void changeExperience (int xp) {
