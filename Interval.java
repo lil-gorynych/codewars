d663fdf78221bc5399c2eee91073c88846d156aa
lil-gorynych <64908967+lil-gorynych@users.noreply.github.com>
2020-09-10 15:27:59 +0300

    Delete Interval.java

--- Interval.java
+++
@@ -1,9 +0,0 @@
-public class Interval {
-
-    public static int sumIntervals(int[][] intervals) {
-        // TODO: implement this method
-        return -1;
-    }
-
-    //private static int intervalLength();
-}
